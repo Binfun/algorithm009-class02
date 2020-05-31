616a7a7bd1691a250f0ad72a5cf1a0e503efd125
binzhao <binfeng.zhao@nokia-sbell.com>
2020-05-31 22:36:05 +0800

    字母异位词分组

---
+++ Week_02/group-anagrams.py
@@ -0,0 +1,18 @@
+import collections
+class Solution(object):
+    def groupAnagrams_count(self, strs):
+        ans = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return list(ans.values())
+
+    def groupAnagrams_sort(self, strs: List[str]) -> List[List[str]]:
+        dict = {}
+        for item in strs:
+            key = tuple(sorted(item))
+            dict[key] = dict.get(key, []) + [item]
+        return list(dict.values())
+
